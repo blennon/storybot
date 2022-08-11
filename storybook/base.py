a964c4b79c8e216c871d4f9545eae19f4318e943
Bill Lennon <blennon86@gmail.com>
2022-08-11 10:44:57 -0700

    generate synopsis outline

--- storybook/base.py
+++ storybook/base.py
@@ -29,7 +29,6 @@ def complete(prompt, params, max_retry=5):
             print('Error communicating with OpenAI:', oops)
             sleep(1)
 
-
 def check_string_list(text: str, min_items: int = 3):
     text = '1.'+text
     if len(text)<10:
@@ -67,8 +66,7 @@ def make_start_prompt(customer_preferences: CustomerPreferences):
     Audience: {customer_preferences.audience}
     Themes: {', '.join(customer_preferences.themes)}
     Tones: {', '.join(customer_preferences.tones)}
-    Main character name: {customer_preferences.main_character_name}
-    """.format(customer_preferences)
+    Main character name: {customer_preferences.main_character_name}""".format(customer_preferences)
     return dedent(prompt)
 
 def generate_response(start_prompt, idea_prompt, params):
@@ -86,14 +84,14 @@ def set_ideas_from_list(start_prompt: str, idea_name: str, ideas: List[str], sel
     return set_idea(start_prompt, idea_name, ideas[selection[0]-1])
 
 def set_idea(start_prompt, idea_name, idea_value):
-    return '\n'.join([start_prompt, f"\n{idea_name}: {idea_value.strip()}"])
+    return '\n'.join([start_prompt, f"\n{idea_name}{idea_value.strip()}"])
 
 def parse_list_selection(text: str) -> List[int]:
     return [int(item) for item in text.split(',')]
 
 def brainstorm_and_set(
     start_prompt: str, ideas_prompt: str, idea_name: str, params: dict, 
-    retries: int = 3, list: bool = False, multiple: bool = False) -> str:
+    retries: int = 5, list: bool = False, multiple: bool = False) -> str:
     """
     Ask GPT3 to brainstorm ideas based on the ideas_prompt.
 
@@ -115,7 +113,7 @@ def brainstorm_and_set(
             logging.warning("GPT3 failed to generate a non-empty response.")
             if retry >= retries:
                 logging.error("#######\nStart Prompt\n#######\n{}".format(start_prompt))
-                return Exception("Too many retries")
+                raise Exception("Too many retries")
             continue
         if list:
             if not check_string_list(text):
@@ -159,12 +157,40 @@ def binary_query(prompt, question, verbose=False):
         return False
     return None
 
+def generate_synopsis(start_prompt: str, params: dict, retries: int = 3) -> str:
+    """
+    Ask GPT3 to generate a synopsis based on the start_prompt.
+    """
+    synopsis_start = "ACT 1\n1."
+    synopsis_prompt = f"Write a detailed synopsis in three Acts.\n\n{synopsis_start}"
+    retry = 0
+    while retry < retries:
+        _, response = generate_response(start_prompt, synopsis_prompt, params)
+        synopsis = synopsis_start+response.choices[0].text
+        logging.debug(synopsis)
+        if check_synopsis(synopsis):
+            return synopsis, '\n'.join([start_prompt, synopsis_prompt + response.choices[0].text])
+        retry += 1
+    raise Exception("Too many retries")
+
+def check_synopsis(text: str) -> bool:
+    """
+    Check if the text is a valid synopsis.
+    """
+    if len(text.strip()) == 0:
+        return False
+    for i in range (3):
+        if f"ACT {i+1}" not in text:
+            return False
+    return True
+
 story_ideas_prompt = "Brainstorm story ideas:\n1."
 antag_ideas_prompt = "Brainstorm antagonist ideas:\n1."
 char_ideas_prompt = "Brainstorm and develop supporting characters:\n1."
 develop_protag_prompt = "Develop the main character:"
 develop_antag_prompt = "Name the antagonist and develop the character and their motivations:"
 question_antag_prompt = "Has an antagonist been described above?"
+
 
 if __name__ == "__main__":
     """
@@ -208,6 +234,11 @@ if __name__ == "__main__":
 
     Then, use EDIT to revise each Act providing the following instructions: Convert ACT {{ACT_NUM}} into a written narrative for a children’s picture book.
     """
+    # TO DO
+    # Experiment with adding plot points
+    # Fix issue with GPT-3 generating a blank antagonist development
+    # Fix issue with GPT-3 generating more than is asked, e.g. multiple lists of ideas
+    # Format the structure using # for headers and use this as a stop signal
     params = {'model':'text-davinci-002',
             'max_tokens':512,
             'temperature':1.0,
@@ -218,7 +249,8 @@ if __name__ == "__main__":
             }
 
     develop_params = params.copy()
-    develop_params['stop'] = ['\n','\r']
+    #develop_params['stop'] = ['\n\n']
+    #develop_params['temperature'] = 0.7
     test = True
     while True:
 
@@ -242,29 +274,34 @@ if __name__ == "__main__":
                 tones=['Whimsical', 'Heartwarming'], 
                 main_character_name='Rowan, a girl.'
             )
-        start_prompt = make_start_prompt(pref)
-        print(start_prompt)
+        prompt = make_start_prompt(pref)
+        print(prompt)
           
         print("\nOkay, now we'll brainstorm some story ideas. Generating...\n")
-        start_prompt = brainstorm_and_set(start_prompt, story_ideas_prompt, "Story idea", params, list=True)
+        prompt = brainstorm_and_set(prompt, story_ideas_prompt, "Story idea: ", params, list=True)
         
-        print("\nGreat, now we'll develop the main character a bit more. What do you think about this...\n")
-        start_prompt = brainstorm_and_set(start_prompt, develop_protag_prompt, "Main character", develop_params)
+        #print("\nGreat, now we'll develop the main character a bit more. What do you think about this...\n")
+        #start_prompt = brainstorm_and_set(start_prompt, develop_protag_prompt, "Main character: ", develop_params)
 
-        answer = binary_query(start_prompt, question_antag_prompt, verbose=True)
+        answer = binary_query(prompt, question_antag_prompt, verbose=True)
         if answer is None or not answer:
             print("\nNow we'll brainstorm some antagonist ideas. Generating...\n")
-            start_prompt = brainstorm_and_set(start_prompt, antag_ideas_prompt, "Antagonist idea", params, list=True)
+            prompt = brainstorm_and_set(prompt, antag_ideas_prompt, "Antagonist idea: ", params, list=True)
 
         print("\nGreat, now we'll develop the antagonist a bit more. What do you think about this...\n")
-        start_prompt = brainstorm_and_set(start_prompt, develop_antag_prompt, "Antagonist", develop_params)
+        prompt = brainstorm_and_set(prompt, develop_antag_prompt, "Antagonist: ", develop_params)
 
         # brainstorm and enumerate supporting characters
-        print(start_prompt)
         print("\nNow we'll brainstorm some supporting characters. Generating...\n")
-        start_prompt = brainstorm_and_set(start_prompt, char_ideas_prompt, "Supporting characters", params, list=True, multiple=True)
-        print(start_prompt)
+        prompt = brainstorm_and_set(prompt, char_ideas_prompt, "Supporting characters: ", params, list=True, multiple=True)
+        print(prompt)
 
         # write a detailed synopsis in three Acts.
+        print("\nNow we'll outline a detailed synopsis in three Acts.\n")
+        synopsis, prompt = generate_synopsis(prompt, params)
+        print(synopsis)
+        with open("./data/synopsis.txt", "w") as f:
+            f.write(prompt)
 
         # revise each Act using EDIT
+        break
